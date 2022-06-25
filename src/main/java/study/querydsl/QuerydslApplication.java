bbb67eed6e63e25150ed0de51544e34b3d39e8c0
hyojhand <sonjw5128@gmail.com>
2022-06-26 01:55:21 +0900

    [Add] JPA with Querydsl

--- src/main/java/study/querydsl/QuerydslApplication.java
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -1,7 +1,11 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class QuerydslApplication {
@@ -9,5 +13,11 @@ public class QuerydslApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuerydslApplication.class, args);
 	}
+
+	// 바로 Bean으로 등록해놓고 사용하는곳에서 주입받아서 사용해도 된다.
+//	@Bean
+//	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+//		return new JPAQueryFactory(em);
+//	}
 
 }
